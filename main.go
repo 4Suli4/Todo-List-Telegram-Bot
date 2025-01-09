479032a5e2fee036b8e6f8d0a69abf7f8268fdb9
4Suli4 <dev@suli4.ru>
2025-01-09 17:17:12 +0500

    Сделал загрузку переменных из .env файла Сделал подключение к Телеграм Боту

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
 func main() {
 
+	errLoadEnv := godotenv.Load()
+	if errLoadEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	}
 }
